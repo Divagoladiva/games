4b23eea0ad62ca00a5689451db22bb57e2444d8a
Divago <divago5452@hotmail.com>
2026-05-16 00:24:23 +0200

    Update games.js

--- games.js
+++ games.js
@@ -4,5 +4,7 @@ const games = [
   { id: 'game-3', title: 'Papazino', description: 'Choisis des cartes, gagne des points, échange ou élimine.', file: 'game-3.html' },
   { id: 'game-4', title: 'Aboubateau', description: 'Trouve les bateaux ennemis sur la grille 10x10.', file: 'game-4.html' },
   { id: 'game-5', title: 'Calcul FF mental', description: 'Réponds rapidement aux calculs, évite l’élimination.', file: 'game-5.html' },
-  { id: 'game-6', title: 'Bouncing Circles', description: 'Choisis une bille puis survive dans un cercle en rotation avec un trou qui grandit.', file: 'game-6.html' }
+  { id: 'game-6', title: 'Bouncing Circles', description: 'Choisis une bille puis survive dans un cercle en rotation avec un trou qui grandit.', file: 'game-6.html' },
+  { id: 'game-7', title: 'Aboubacarrés', description: 'Fais se combattre tes carrés préférés dans une arène.', file: 'game-7.html' },
+  { id: 'game-8', title: 'Aboublack-Jack', description: 'Ne dépasse pas 21 et fais un max de flouz mon gaté!', file: 'game-8.html' }
 ];
