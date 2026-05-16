9f22358d543f7c718822b9d748db0342f760675a
Divago <divago5452@hotmail.com>
2026-05-16 18:57:30 +0200

    Refactor player management and UI interactions

--- script.js
+++ script.js
@@ -214,7 +214,7 @@ function closeOverlay() {
   if (randomContent) randomContent.style.visibility = 'visible';
 }
 
-const gameColors = ['#ff5f72','#6a82fb','#48c6ef','#7bffb1','#ffbd55','#e96dff','#ff8c42','#6b6b6b','#2e1197', '#f8fa6f'];
+const gameColors = ['#ff5f72','#6a82fb','#48c6ef','#7bffb1','#ffbd55','#e96dff','#ff8c42','#6b6b6b','#2e1197', '#f8fa6f', '#fdfdfd'];
 
 function showGameListOverlay() {
   mainContainer.classList.add('hidden');
@@ -278,7 +278,7 @@ function drawWheel(angle, gameList = games) {
     ctx.arc(center, center, radius, startA, endA);
     ctx.closePath();
 
-    const fillPalette = ['#ff5f72', '#6a82fb', '#48c6ef', '#7bffb1', '#ffbd55', '#e96dff', 'rgb(255, 106, 163)', 'rgb(255, 155, 116)', 'rgb(132, 255, 116)', 'rgb(165, 116, 255)'];
+    const fillPalette = ['#ff5f72', '#6a82fb', '#48c6ef', '#7bffb1', '#ffbd55', '#e96dff', 'rgb(255, 106, 163)', 'rgb(255, 155, 116)', 'rgb(132, 255, 116)', 'rgb(165, 116, 255)', 'rgb(234, 236, 119)'];
     ctx.fillStyle = fillPalette[index % fillPalette.length];
     ctx.fill();
 
