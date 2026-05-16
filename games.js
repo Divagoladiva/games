1d4707d5ec2bc99340268384ab1a93d83cd574cc
Divago <divago5452@hotmail.com>
2026-05-16 12:11:54 +0200

    Update games.js

--- games.js
+++ games.js
@@ -6,5 +6,7 @@ const games = [
   { id: 'game-5', title: 'Calcul FF mental', description: 'Réponds rapidement aux calculs, évite l’élimination.', file: 'game-5.html' },
   { id: 'game-6', title: 'Bouncing Circles', description: 'Choisis une bille puis survive dans un cercle en rotation avec un trou qui grandit.', file: 'game-6.html' },
   { id: 'game-7', title: 'Aboubacarrés', description: 'Fais se combattre tes carrés préférés dans une arène.', file: 'game-7.html' },
-  { id: 'game-8', title: 'Aboublack-Jack', description: 'Ne dépasse pas 21 et fais un max de flouz mon gaté!', file: 'game-8.html' }
+  { id: 'game-8', title: 'Aboublack-Jack', description: 'Ne dépasse pas 21 et fais un max de flouz mon gaté!', file: 'game-8.html' },
+  { id: 'game-9', title: 'Aboujackpot', description: 'Tourne tourne tourne et gagne des millions!', file: 'game-9.html' },
+  { id: 'game-10', title: 'Aboubakar Burger', description: 'Mange un max de burger et évite les ping pongs', file: 'game-10.html' }
 ];
